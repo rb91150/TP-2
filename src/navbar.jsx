0a6b5f1ed55b202aaaa46810e39de771059d6566
rb.91150 <raphael.boeebosale.pro@gmail.com>
2024-11-07 12:06:08 +0100

    v1

---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+import { link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            {
+             
+             }
+                <Link to='/'> Accueil !</Link>
+                <Link to='/page1'>Page 1 </Link>
+            
+        </nav>
+    );
+}
+
+export default Navbar
